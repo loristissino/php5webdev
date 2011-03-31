3f12d75c9885a314aa46bae7dfd3a82517809221
loris.tissino <loris.tissino@a848e3ed-2138-fbc9-04e2-a3e0ee45af58>
2011-03-31 16:26:09 +0000

    modificato esempio

--- web/lesson02/costrutti06.php
+++ web/lesson02/costrutti06.php
@@ -1,13 +1,21 @@
 <pre style="color:blue">
 <?php
 
-$testo="abc;def ghi";
-
-list($codice, $nome)=explode(';', $testo);
-
-echo "codice: $codice, nome: $nome\n";
+//Obiettivo: prendere il file e per ogni riga
+//la prima parte sarà la chiave dell'elemento dell'array
+//e la seconda il valore
 
 $nazioni=file('nazioniconcodice.txt');
 
 print_r($nazioni);
+
+$nazioniconcodice=array(); // inizializzo un array vuoto
+foreach($nazioni as $nazione)
+{
+  list($codice, $valore)=explode(';',$nazione);
+  $nazioniconcodice[$codice]=$valore;
+  // metto nell'array l'elemento corrente
+}
+
+print_r($nazioniconcodice);
 
