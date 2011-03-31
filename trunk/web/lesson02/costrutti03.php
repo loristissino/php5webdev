2b49b272cc9597a9ddbe4e55222388bfaa4096e6
loris.tissino <loris.tissino@a848e3ed-2138-fbc9-04e2-a3e0ee45af58>
2011-03-31 15:01:36 +0000

    added example 3

---
+++ trunk/web/lesson02/costrutti03.php
@@ -0,0 +1,24 @@
+<pre style="color:blue">
+<?php
+
+
+$lista=array(
+  'it'=>'Italia', 
+  'gb'=>'Regno Unito', 
+  'fr'=>'Francia', 
+  'de'=>'Germania',
+  );
+
+print_r($lista);
+
+sort($lista);
+
+print_r($lista);
+
+ksort($lista);
+
+print_r($lista);
+
+
+
+
