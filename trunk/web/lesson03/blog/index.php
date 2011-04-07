16507c638db2ca0c73d2c329b9f5bd6c804b36c7
loris.tissino <loris.tissino@a848e3ed-2138-fbc9-04e2-a3e0ee45af58>
2011-04-07 15:57:58 +0000

    seconda versione blog

--- trunk/web/lesson03/blog/index.php
+++ trunk/web/lesson03/blog/index.php
@@ -1,5 +1,27 @@
 <pre>
 <?php
 
-  $posts=scandir('posts'); // leggo la directory dei post
-  print_r($posts); // visualizzo l'elenco
+  const POSTS_DIR='posts';
+
+  $posts=scandir(POSTS_DIR); // leggo la directory dei post
+  //print_r($posts); // visualizzo l'elenco
+
+  $realposts=array();
+  foreach($posts as $post)
+  {
+    if (substr($post,0,1)!='.')
+    {
+      $realposts[]=$post;
+    }
+  }
+  
+  //print_r($realposts);
+  
+?>
+</pre>
+<h1>Elenco post</h1>
+<ul>
+<?php foreach($realposts as $post): ?>
+  <li><?php echo $post ?></li>
+<?php endforeach ?>
+</ul>
