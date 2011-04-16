0a399d834055aadade065d2d14c22e588e9f1d4d
loris.tissino <loris.tissino@a848e3ed-2138-fbc9-04e2-a3e0ee45af58>
2011-04-16 09:57:03 +0000

    updated lib

--- web/lesson03/lib/useful_functions.php
+++ web/lesson03/lib/useful_functions.php
@@ -19,3 +19,41 @@ function nothidden($filename)
 {
   return substr(basename($filename), 0, 1)!='.';
 }
+
+
+/**
+ * save a post using global array $_GET and $_POST as input
+ *
+ * We assume that the $_GET and $_POST variables contain the values
+ * of the post to update.
+ * No check is done before saving the file.
+ *
+ * @author Loris Tissino <loris@tissino.it> 
+ * @return bool true if the post was correctly saved
+ */
+function savepost()
+{
+  // FIXME this must be written!
+  
+  return true;
+}
+
+/**
+ * redirect the browser to a page specified by $target
+ *
+ * This function is useful when you want to redirect the browser after a post.
+ * See http://php.net/manual/en/function.header.php for details.
+ *
+ * @author Loris Tissino <loris@tissino.it> 
+ * @return bool true if the post was correctly saved
+ */
+function redirect($target, $message='')
+{
+  $host  = $_SERVER['HTTP_HOST'];
+  $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+  $extra = $target;
+  
+  setcookie('message', $message, time()+3600);
+  header("Location: http://$host$uri/$extra");
+  exit;
+}
